5f3d4bddf494ce56873510b65ef7171c3654e6cb
wasifAhmed15 <wasifahmedkhan99@gmail.com>
2023-07-09 16:05:23 +0500

    formValue

---
+++ node.js
@@ -0,0 +1,17 @@
+
+function signup(){
+
+let Name = document.getElementById("Name").value
+let username= document.getElementById("username").value
+let contact = document.getElementById("contact").value
+let password = document.getElementById("password").value
+let Month = document.getElementById("Month").value
+let dat = document.getElementById("Dates").value
+let years = document.getElementById("year").value
+let male = document.getElementById("male").value
+let female = document.getElementById("female").value
+
+    console.log(Name,username,contact, password, Month,dat,years,male,female)
+
+
+}
